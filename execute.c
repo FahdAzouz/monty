d21a903615b2e4c2544e5962162af3224e98da16
Fahd Azouz <fahdstudies00@gmail.com>
2023-09-21 22:56:01 +0100

    execute

--- execute.c
+++ execute.c
@@ -9,7 +9,7 @@
 */
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-	instruction_t opst[] = {
+	instruction_t _op[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
 				{"pop", f_pop},
 				{"swap", f_swap},
@@ -27,22 +27,23 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"stack", f_stack},
 				{NULL, NULL}
 				};
-	unsigned int i = 0;
 	char *op;
+	unsigned int j = 0;
 
 	op = strtok(content, " \n\t");
-	if (op && op[0] == '#')
+	if (op[0] == '#' && op)
 		return (0);
 	bus.arg = strtok(NULL, " \n\t");
-	while (opst[i].opcode && op)
+	while (op && _op[j].opcode)
 	{
-		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, counter);
+		if (strcmp(op, _op[j].opcode) == 0)
+		{	_op[j].f(stack, counter);
 			return (0);
 		}
-		i++;
+		j++;
 	}
-	if (op && opst[i].opcode == NULL)
+
+	if (_op[j].opcode == NULL && op)
 	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
 		free(content);
